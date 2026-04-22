b970f47de4a0234a977ef2ece5570656be07e979
Salah Eddine Belhaska <salaheddine.belhaska@sio-carriat.com>
2026-04-22 14:06:26 +0200

    ajoute de route dans app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import { Accueil } from "./pages/Accueil";
 import { Login } from "./pages/Login";
 import { Footer } from "./components/Footer";
+import { Admin } from "./pages/Admin";
+import { Missions } from "./pages/Missions";
+
 
 export const App = () => {
   return (
@@ -9,6 +12,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Admin" element={<Admin />}/>
+        <Route path="/Missions" element={<Missions />} />
       </Routes>
       <Footer />
     </div>
