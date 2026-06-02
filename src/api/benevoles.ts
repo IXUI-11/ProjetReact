1ab9c4e000c2e9fc99df8061e28d91e9c217a3c6
IXUI-11 <lazalasa1990@gmail.com>
2026-06-02 12:08:58 +0200

    feat: ajout des appels API pour la gestion des bénévoles et participations
    - Ajout de la fonction `supprimerBenevole` pour supprimer un bénévole.
    - Ajout de la fonction `ajouterBenevole` pour la création d'un nouveau profil.
    - Ajout de la fonction `getParticipationsBenevole` pour récupérer la liste des participations d'un bénévole (partie Admin).
    - Ajout de la fonction `annulerParticipationAdmin` pour permettre l'annulation d'une participation.

--- src/api/benevoles.ts
+++ src/api/benevoles.ts
@@ -4,3 +4,37 @@ export async function getTousLesBenevoles() {
     const reponse = await fetch(BASE_URL)
     return reponse.json()
 }
+
+export async function supprimerBenevole(id: number) {
+    const reponse = await fetch(`${BASE_URL}?action=supprimerBenevole`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id })
+    });
+    return reponse.json();
+};
+
+export async function ajouterBenevole(nom: string, prenom: string, email: string, mot_de_passe: string, adresse: string, code_postal: string, date_naissance: string) {
+    const reponse = await fetch(`${BASE_URL}?action=ajouterBenevole`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nom, prenom, email, mot_de_passe, adresse, code_postal, date_naissance })
+    });
+    return reponse.json();
+}
+
+// ! Partie Admin Récupérer les participations d'un bénévole
+export async function getParticipationsBenevole(id: number) {
+    const reponse = await fetch(`${BASE_URL}?action=getParticipations&id=${id}`);
+    return reponse.json();
+}
+
+// ! Partie Admin pour annuler une participation d'un bénévole
+export async function annulerParticipationAdmin(id: number) {
+    const reponse = await fetch(`http://localhost/api/Controller/participation.php?action=annulerAdmin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id })
+    });
+    return reponse.json();
+}
