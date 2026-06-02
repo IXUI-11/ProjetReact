f7cb3bcb62a280657313007e5c4f04f7e03a9dd5
IXUI-11 <lazalasa1990@gmail.com>
2026-06-02 12:09:27 +0200

    feat: ajout d'appel API pour l'inscription

---
+++ src/api/inscription.ts
@@ -0,0 +1,10 @@
+const BASE_URL = "http://localhost/api/Controller/inscription.php";
+
+export async function creerCompte(nom: string, prenom: string, email: string, mot_de_passe: string, adresse: string, code_postal: string, date_naissance: string) {
+    const reponse = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nom, prenom, email, mot_de_passe, adresse, code_postal, date_naissance })
+    });
+    return reponse.json();
+}
