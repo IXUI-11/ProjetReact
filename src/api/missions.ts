86d295fb1958f19390e85a2884a8e1c77db53e1c
IXUI-11 <lazalasa1990@gmail.com>
2026-06-02 12:10:33 +0200

    feat: ajout des appels API CRUD pour la gestion des missions (Admin)
    - Ajout de la fonction `ajouterMission` pour créer une nouvelle mission.
    - Ajout de la fonction `supprimerMission` pour effacer une mission via son ID.
    - Ajout de la fonction `modifierMission` pour mettre à jour les détails (titre, lieu, description, date).
    - Ajout de la fonction `changerStatutMission` pour basculer l'état d'une mission (active ou terminée).

--- src/api/missions.ts
+++ src/api/missions.ts
@@ -9,3 +9,54 @@ export async function getMissionsRecentes() {
     const reponse = await fetch(`${BASE_URL}?action=MissionRecent`)
     return reponse.json()
 }
+
+// Partie Admin pour ajouter une mission 
+export async function ajouterMission(titre: string, lieu: string, description: string, date_mission: string) {
+    const reponse = await fetch(`${BASE_URL}?action=ajouterMission`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            titre: titre,
+            lieu: lieu,
+            description: description,
+            date_mission: date_mission
+        })
+    });
+    return reponse.json();
+}
+
+// Partie admin pour supprimer une mission
+export async function supprimerMission(id: number) {
+    const reponse = await fetch(`${BASE_URL}?action=supprimerMission`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id })
+    });
+    return reponse.json();
+}
+
+// Partie Admin modifer une mission
+export async function modifierMission(id: number, titre: string, lieu: string, description: string, date_mission: string) {
+    const reponse = await fetch(`${BASE_URL}?action=modifierMission`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: id,
+            titre: titre,
+            lieu: lieu,
+            description: description,
+            date_mission: date_mission
+        })
+    });
+    return reponse.json();
+}
+
+// Partie Admin pour changer le statut d'une mission (active ou finie)
+export async function changerStatutMission(id: number, actif: number) {
+    const reponse = await fetch(`${BASE_URL}?action=changerStatut`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id, actif })
+    });
+    return reponse.json();
+}
